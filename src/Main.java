6a8e9ef5e22eddad2b9281e375dfbc94b7319160
Alexandre Mercier <alex.mercier@live.ca>
2025-02-21 15:27:21 -0500

    Ajout des bases pour prendre une entrée et avoir une sortie. Le critère d'acception des espaces dans l'expression est géré.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Entrez l'expression: ");
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.nextLine().replace(" ", "");
+        scanner.close();
+        System.out.println("Résultat: " + expression);
     }
 }
