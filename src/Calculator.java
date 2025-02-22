02c535bae796d8180ebf83e44989224abadb93c4
Alexandre Mercier <alex.mercier@live.ca>
2025-02-21 22:26:10 -0500

    Ajout de l'exposant. Même logique que pour la multiplication et la division mais je rajoute une couche.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,4 @@
-import Operations.Addition;
-import Operations.Division;
-import Operations.Multiplication;
-import Operations.Substaction;
+import Operations.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,69 +16,99 @@ public class Calculator {
                 throw new Exception("Il faut utiliser un point(.) et non une virgule(,) pour les nombres décimaux.");
             }
 
-            double result = calculate(expression);
-            System.out.println("Résultat: " + result);
+            Double result = calculate(expression);
+            if(result != null) {
+                System.out.println("Résultat: " + result);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private static double calculate(String expression) throws Exception {
-        List<String> numbers = new ArrayList<>();
-        List<String> operators = new ArrayList<>();
-        String number = "";
+    private static Double calculate(String expression) throws Exception {
+        try{
+            List<String> numbers = new ArrayList<>();
+            List<String> operators = new ArrayList<>();
+            String number = "";
 
-        for(char character : expression.toCharArray()) {
-            if(Character.isDigit(character) || character == '.') {
-                number += character;
-            }
-            else if (character == '+' || character == '-' || character == '*' || character == '/') {
-                if(number.isEmpty() && character == '-') {
-                    number+=character;
+            for(char character : expression.toCharArray()) {
+                if(Character.isDigit(character) || character == '.') {
+                    number += character;
+                }
+                else if (character == '+' || character == '-' || character == '*' || character == '/' || character == '^') {
+                    if(number.isEmpty() && character == '-') {
+                        number+=character;
+                    }
+                    else{
+                        numbers.add(number);
+                        number = "";
+                        operators.add(String.valueOf(character));
+                    }
                 }
                 else{
-                    numbers.add(number);
-                    number = "";
-                    operators.add(String.valueOf(character));
+                    throw new Exception("Il y a un caractère invalide.");
                 }
             }
-            else{
-                throw new Exception("Il y a un caractère invalide.");
+            numbers.add(number);
+
+            List<String> newNumbers1 = new ArrayList<>();
+            List<String> newOperators1 = new ArrayList<>();
+            String firstNumber = numbers.getFirst();
+
+            for(int i=0;i<operators.size();i++) {
+                String secondNumber = numbers.get(i+1);
+                if(operators.get(i).equals("^")) {
+                    firstNumber = String.valueOf(Exponent.exponent(firstNumber,secondNumber));
+                }
+                else{
+                    newNumbers1.add(firstNumber);
+                    newOperators1.add(operators.get(i));
+                    firstNumber = secondNumber;
+                }
             }
+            newNumbers1.add(firstNumber);
+
+            List<String> newNumbers2 = new ArrayList<>();
+            List<String> newOperators2 = new ArrayList<>();
+            firstNumber = newNumbers1.getFirst();
+
+            for(int i=0;i<newOperators1.size();i++) {
+                String secondNumber = newNumbers1.get(i+1);
+                if(newOperators1.get(i).equals("*")) {
+                    firstNumber = String.valueOf(Multiplication.multiply(firstNumber,secondNumber));
+                }
+                else if(newOperators1.get(i).equals("/")) {
+                    if(Double.parseDouble(secondNumber)==0) {
+                        throw new Exception("Il est impossible de diviser par zéro.");
+                    }
+                    else{
+                        firstNumber = String.valueOf(Division.divide(firstNumber,secondNumber));
+                    }
+                }
+                else{
+                    newNumbers2.add(firstNumber);
+                    newOperators2.add(newOperators1.get(i));
+                    firstNumber = secondNumber;
+                }
+            }
+            newNumbers2.add(firstNumber);
+
+            firstNumber = newNumbers2.getFirst();
+            for(int i=0;i<newOperators2.size();i++) {
+                String secondNumber = newNumbers2.get(i+1);
+                if(newOperators2.get(i).equals("+")) {
+                    firstNumber = String.valueOf(Addition.add(firstNumber,secondNumber));
+                }
+                else if(newOperators2.get(i).equals("-")) {
+                    firstNumber = String.valueOf(Substaction.substract(firstNumber,secondNumber));
+                }
+            }
+
+            return Double.parseDouble(firstNumber);
         }
-        numbers.add(number);
-
-        List<String> newNumbers = new ArrayList<>();
-        List<String> newOperators = new ArrayList<>();
-        String firstNumber = numbers.get(0);
-
-        for(int i=0;i<operators.size();i++) {
-            String secondNumber = numbers.get(i+1);
-            if(operators.get(i).equals("*")) {
-                firstNumber = String.valueOf(Multiplication.multiply(firstNumber,secondNumber));
-            }
-            else if(operators.get(i).equals("/")) {
-                firstNumber = String.valueOf(Division.divide(firstNumber,secondNumber));
-            }
-            else{
-                newNumbers.add(firstNumber);
-                newOperators.add(operators.get(i));
-                firstNumber = secondNumber;
-            }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            return null;
         }
-        newNumbers.add(firstNumber);
-
-        firstNumber = newNumbers.get(0);
-        for(int i=0;i<newOperators.size();i++) {
-            String secondNumber = newNumbers.get(i+1);
-            if(newOperators.get(i).equals("+")) {
-                firstNumber = String.valueOf(Addition.add(firstNumber,secondNumber));
-            }
-            else if(newOperators.get(i).equals("-")) {
-                firstNumber = String.valueOf(Substaction.substract(firstNumber,secondNumber));
-            }
-        }
-
-        return Double.parseDouble(firstNumber);
     }
 }
